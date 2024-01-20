a9cc8438969aa38e285bad26dd2206c6107695f1
SundeevAlex <151011410+SundeevAlex@users.noreply.github.com>
2024-01-20 16:36:37 +0700

    Delete str_func.py

--- str_func.py
+++
@@ -1,8 +0,0 @@
-def switch_upper(word):
-    """ переводит в заглавные буквы """
-    return word.upper()
-
-
-def switch_title(word):
-    """ делает заглавными первые буквы """
-    return word.title()
